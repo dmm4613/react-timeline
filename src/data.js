8aecac4fc72c4ea24ff9b691e9c62689c0807de4
Dan <dmm4613@gmail.com>
2020-01-27 13:00:47 -0500

    this is a timeline react component I found through florin pop

---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    text: 'Started working on Florin Pop\'s projects',
+    date: 'Janurary 27 2020',
+    category: {
+      tag: 'JavaScript',
+      color: '#D4807B'
+    },
+    link: {
+      url:
+        'https://codepen.io/dmm4613',
+      text: 'See my work'
+    }
+  },
+  {
+    text: 'Landed my first tech job',
+    date: 'August 19 2019',
+    category: {
+      tag: 'jobs',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://www.linkedin.com/in/dan-miller-5138b421/',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Graduated from Momentum Learning',
+    date: 'May 4 2019',
+    category: {
+      tag: 'Education',
+      color: '#509DEB'
+    },
+    link: {
+      url:
+        'https://momentumlearn.com',
+      text: 'Check them out'
+    }
+  }
+];
+
+export default data;
